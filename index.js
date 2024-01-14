cd0d76cb6782a2bd84af6e5ce61376169b8f1a7b
Prasanna Kundu <pkundu307@gmail.com>
2024-01-14 22:54:20 +0530

    check

--- index.js
+++ index.js
@@ -72,42 +72,42 @@ const upload = multer({ storage: storage });
 
 const endpointSecret = process.env.ENDPOINT_SECRET;
 
-server.post(
-  '/webhook',
-  express.raw({ type: 'application/json' }),
-  async (request, response) => {
-    const sig = request.headers['stripe-signature'];
+// server.post(
+//   '/webhook',
+//   express.raw({ type: 'application/json' }),
+//   async (request, response) => {
+//     const sig = request.headers['stripe-signature'];
 
-    let event;
+//     let event;
 
-    try {
-      event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
-    } catch (err) {
-      response.status(400).send(`Webhook Error: ${err.message}`);
-      return;
-    }
+//     try {
+//       event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
+//     } catch (err) {
+//       response.status(400).send(`Webhook Error: ${err.message}`);
+//       return;
+//     }
 
-    // Handle the event
-    switch (event.type) {
-      case 'payment_intent.succeeded':
-        const paymentIntentSucceeded = event.data.object;
+//     // Handle the event
+//     switch (event.type) {
+//       case 'payment_intent.succeeded':
+//         const paymentIntentSucceeded = event.data.object;
 
-        const order = await Order.findById(
-          paymentIntentSucceeded.metadata.orderId
-        );
-        order.paymentStatus = 'received';
-        await order.save();
+//         const order = await Order.findById(
+//           paymentIntentSucceeded.metadata.orderId
+//         );
+//         order.paymentStatus = 'received';
+//         await order.save();
 
-        break;
-      // ... handle other event types
-      default:
+//         break;
+//       // ... handle other event types
+//       default:
 
-    }
+//     }
 
-    // Return a 200 response to acknowledge receipt of the event
-    response.send();
-  }
-);
+//     // Return a 200 response to acknowledge receipt of the event
+//     response.send();
+//   }
+// );
 
 // JWT options
 
